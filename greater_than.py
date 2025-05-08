49108ce1e5c42e007848dc01e83307048c9f483c
Rdeleon32 <rdeleon5@ivytech.edu>
2025-05-08 13:34:42 -0400

    Add files via upload

---
+++ greater_than.py
@@ -0,0 +1,6 @@
+def greater_than(x, y):
+    return x > y  # Returns True if x is greater than y, otherwise False
+a = 10
+b = 6
+result = greater_than(a, b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
